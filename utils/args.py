8b069417d1b768a516a9ffb9540e0257f944b698
costoxff <900131shawn@gmail.com>
2025-11-26 21:24:29 +0800

    Fix argument error type

--- utils/args.py
+++ utils/args.py
@@ -4,5 +4,5 @@ def parse_arguments() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument('--host', type=str, default='0.0.0.0')
     parser.add_argument('--port', type=int, default=25565)
-    parser.add_argument('--model', type=int, default='llama3')
+    parser.add_argument('--model', type=str, default='llama3')
     return parser.parse_args()
